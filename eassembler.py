ca4303cfd3b6a0261305e06303a4c90f371bb25e
Renata Cavalheiro da Silva <recavalheiro89@gmail.com>
2018-09-03 12:32:57 -0300

    Add files via upload

---
+++ eassembler.py
@@ -0,0 +1,20 @@
+def openfile():
+    user_input = input("Entre com o nome do arquivo: ")
+    opcodes = '/Users/renata.cavalheiro/OPCODES.esym'
+    regcodes = '/Users/renata.cavalheiro/REGCODES.esym'
+    if user_input == 'opcodes':
+        opcodes_open = open(opcodes, 'r')
+        print(opcodes_open.read())
+        opcodes_open.close()
+    elif user_input == 'regcodes':
+        regcodes_open = open(regcodes, 'r')
+        print(regcodes_open.read())
+        regcodes_open.close()
+    
+    
+    
+    
+    
+    
+                
+    

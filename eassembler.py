61b342d620bac3388fbc0be1dca57bf2cd97baeb
Renata Cavalheiro da Silva <recavalheiro89@gmail.com>
2018-09-10 12:45:20 -0300

    Update eassembler.py

--- eassembler.py
+++ eassembler.py
@@ -1,4 +1,4 @@
-def openfile():
+def assemble():
     """
     função para ler os arquivos para o assembler
     """
@@ -8,16 +8,44 @@ def openfile():
     if user_input == 'opcodes':
         opcodes_open = open(opcodes, 'r')
         print(opcodes_open.read())
-        opcodes_open.close()
+        #opcodes_open.close()
     elif user_input == 'regcodes':
         regcodes_open = open(regcodes, 'r')
         print(regcodes_open.read())
-        regcodes_open.close()
-    
+        #regcodes_open.close()
 
-    
-    
-    
+def dictionary():
+    d = {
+        "NOP": 0,
+        "HALT": 1,
+        "MOV_RR": 2,
+        "MOV_RM": 3,
+        "MOV_MR": 4,
+        "MOV_RI": 5,
+        "MOV_MI": 6,
+        "ADD": 7,
+        "SUB": 8,
+        "CMP": 9,
+        "JMP": 10,
+        "JZ": 11,
+        "JG": 12,
+        "JL": 13,
+        "OUT": 14,
+        "INC": 15,
+        "DEC": 16,
+        "MUL": 17,
+        "DIV": 18
+        }
+    #opcodes = '/Users/renata.cavalheiro/OPCODES.esym'
+    #code = open(opcodes, 'r')
+    #code.read()
+    code = ["NOP"]
+    for i in d:
+        for line in code:
+            for item in d:
+                if line == item:
+                    position = d[i]
+                    print(position)
     
                 
     
